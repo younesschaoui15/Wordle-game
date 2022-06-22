7bf63673a872951f5e7dfb91c9b0998682c7e2f6
CHAOUI Youness <youness.chaoui@ext.europarl.europa.eu>
2022-06-22 15:09:06 +0200

    Add Tailwind CSS framework

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+\
+,.idea/inspectionProfiles/Project_Default.xml,4\9\496a238a6afa168dbaf6efd37bb459331589579c
--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p className={'bg-white rounded-xl text-slate-400 px-8 text-xl'}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
         >
           Learn React
         </a>
